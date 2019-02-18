639b5a751386fa1dac963fc91e6aba0e238a350b
Administrator <Administrator@DESKTOP-KF3JISI.fritz.box>
2019-02-18 18:53:20 +0000

    The Base MVC layout with folder names

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> The Sports Calendar, Inc.
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>The Sports Calendar</title>
+    <link href="../main.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>The Sports Calendar</h1></header>
+       
