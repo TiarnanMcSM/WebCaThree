94d2eaedb55d99bd1b1ba65c78b813a29b42ed06
Administrator <Administrator@DESKTOP-KF3JISI.fritz.box>
2019-02-25 20:31:58 +0000

    Commit 05: I have updated both the event_db and sport_db files within the model for accessing the DB.

--- model/event_db.php
+++ model/event_db.php
@@ -1,8 +1,80 @@
 <?php
+function get_events() {
+    global $db;
+    $query = 'SELECT * FROM events
+              ORDER BY eventID';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $events = $statement->fetchAll();
+    $statement->closeCursor();
+    return $events;
+}
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function get_events_by_sport($sport_id) {
+    global $db;
+    $query = 'SELECT * FROM events
+              WHERE events.sportID = :sport_id
+              ORDER BY eventID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":sport_id", $sport_id);
+    $statement->execute();
+    $events = $statement->fetchAll();
+    $statement->closeCursor();
+    return $events;
+}
 
+function get_event($event_id) {
+    global $db;
+    $query = 'SELECT * FROM events
+              WHERE eventID = :event_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":event", $event_id);
+    $statement->execute();
+    $event = $statement->fetch();
+    $statement->closeCursor();
+    return $event;
+}
+
+function delete_event($event_id) {
+    global $db;
+    $query = 'DELETE FROM events
+              WHERE eventID = :event_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':event_id', $event_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function add_product($sport_id, $name, $date, $location) {
+    global $db;
+    $query = 'INSERT INTO events
+                 (sportID, eventName, startDate, location)
+              VALUES
+                 (:sport_id, :name, :date, :location)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':sport_id', $sport_id);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':date', $date);
+    $statement->bindValue(':location', $location);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function update_product($event_id, $sport_id, $name, $date, $location) {
+    global $db;
+    $query = 'UPDATE events
+              SET sportID = :sport_id,
+                  eventName = :name,
+                  startDate = :date,
+                  location = :location
+               WHERE eventID = :event_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':sport_id', $sport_id);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':date', $date);
+    $statement->bindValue(':location', $location);
+    $statement->bindValue(':event_id', $event_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
+?>
--- model/sport_db.php
+++ model/sport_db.php
@@ -1,8 +1,44 @@
 <?php
+function get_sports() {
+    global $db;
+    $query = 'SELECT * FROM sports
+              ORDER BY sportID';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    return $statement; 
+}
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function get_sport_name($sport_id) {
+    global $db;
+    $query = 'SELECT * FROM sports
+              WHERE sportID = :sport_id';    
+    $statement = $db->prepare($query);
+    $statement->bindValue(':sport_id', $sport_id);
+    $statement->execute();    
+    $category = $statement->fetch();
+    $statement->closeCursor();    
+    $sport_name = $sport['sportName'];
+    return $sport_name;
+}
+
+function add_sport($name) {
+    global $db;
+    $query = 'INSERT INTO sports (sportName)
+              VALUES (:name)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':name', $name);
+    $statement->execute();
+    $statement->closeCursor();    
+}
+
+function delete_sport($sport_id) {
+    global $db;
+    $query = 'DELETE FROM sports
+              WHERE sportID = :sport_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':sport_id', $sport_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
+?>
 
