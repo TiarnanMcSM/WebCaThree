57dbd01ef12a76cbb18d11c742aef4df31206e32
Administrator <Administrator@DESKTOP-KF3JISI.fritz.box>
2019-02-27 20:59:08 +0000

    Commit 11: I have updated my event_add page with base code for testing

--- events_manager/event_add.php
+++ events_manager/event_add.php
@@ -1,8 +1,38 @@
-<?php
+<?php include '../view/header.php'; ?>
+<main>
+    <h1>Add Event</h1>
+    <form action="index.php" method="post" id="add_event_form">
+        <input type="hidden" name="action" value="add_event">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <label>Sport:</label>
+        <select name="sport_id">
+        <?php foreach ( $sports as $sport ) : ?>
+            <option value="<?php echo $sport['sportID']; ?>">
+                <?php echo $sport['sportName']; ?>
+            </option>
+        <?php endforeach; ?>
+        </select>
+        <br>
 
+        <label>Event Name:</label>
+        <input type="input" name="name">
+        <br>
+
+        <label>Start Date:</label>
+        <input type="input" name="date">
+        <br>
+
+        <label>Location:</label>
+        <input type="input" name="location">
+        <br>
+
+        <label>&nbsp;</label>
+        <input type="submit" value="Add Event">
+        <br>
+    </form>
+    <p class="last_paragraph">
+        <a href="index.php?action=list_events">View Events List</a>
+    </p>
+
+</main>
+<?php include '../view/footer.php'; ?>
