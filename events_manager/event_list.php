66288d042973ff4df75a1692ab641187b981053e
Administrator <Administrator@DESKTOP-KF3JISI.fritz.box>
2019-02-27 20:50:01 +0000

    Commit 09: I have updated both the event_list page and the sport_list page for use.

--- events_manager/event_list.php
+++ events_manager/event_list.php
@@ -1,8 +1,54 @@
-<?php
+<?php include '../view/header.php'; ?>
+<main>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <h1>Event List</h1>
 
+    <aside>
+        <!-- display a list of sports -->
+        <h2>Sports</h2>
+        <?php include '../view/sport_nav.php'; ?>        
+    </aside>
+
+    <section>
+        <!-- display a table of events -->
+        <h2><?php echo $sport_name; ?></h2>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Location</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+            </tr>
+            <?php foreach ($events as $event) : ?>
+            <tr>
+                <td><?php echo $event['eventName']; ?></td>
+                <td><?php echo $event['startDate']; ?></td>
+                <td><?php echo $event['location']; ?></td>
+                <td><form action="." method="post">
+                    <input type="hidden" name="action"
+                           value="show_edit_form">
+                    <input type="hidden" name="event_id"
+                           value="<?php echo $event['eventID']; ?>">
+                    <input type="hidden" name="sport_id"
+                           value="<?php echo $event['sportID']; ?>">
+                    <input type="submit" value="Edit">
+                </form></td>
+                <td><form action="." method="post">
+                    <input type="hidden" name="action"
+                           value="delete_event">
+                    <input type="hidden" name="event_id"
+                           value="<?php echo $event['eventID']; ?>">
+                    <input type="hidden" name="sport_id"
+                           value="<?php echo $event['sportID']; ?>">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <p><a href="?action=show_add_form">Add Event</a></p>
+        <p><a href="?action=list_categories">List Sports</a></p>
+    </section>
+
+</main>
+<?php include '../view/footer.php'; ?>
--- events_manager/sport_list.php
+++ events_manager/sport_list.php
@@ -1,8 +1,40 @@
-<?php
+<?php include '../view/header.php'; ?>
+<main>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <h1>Sport List</h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>&nbsp;</th>
+        </tr>
+        <?php foreach ($sports as $sport) : ?>
+        <tr>
+            <td><?php echo $sport['sportName']; ?></td>
+            <td>
+                <form id="delete_event_form"
+                      action="index.php" method="post">
+                    <input type="hidden" name="action" value="delete_sport">
+                    <input type="hidden" name="sport_id"
+                           value="<?php echo $sport['sportID']; ?>">
+                    <input type="submit" value="Delete">
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <br />
 
+    <h2>Add Sport</h2>
+    <form id="add_sport_form"
+          action="index.php" method="post">
+        <input type="hidden" name="action" value="add_sport">
+
+        <label>Sport Name:</label>
+        <input type="input" name="name">
+        <input type="submit" value="Add">
+    </form>
+
+    <p><a href="index.php?action=list_events">List Events</a></p>
+
+</main>
+<?php include '../view/footer.php'; ?>
